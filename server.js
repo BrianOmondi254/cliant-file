dbde020e1b57c2bf1bfe4fe6fc8f80a1b9c298b6
BrianOmondi254 <omondibrianokeyo@gmail.com>
2026-03-29 23:25:50 +0300

    Fixed routes and updated proceedings

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 /* ================= STATIC FILES FOR MOBILE ================= */
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
 
 /* ================= PWA HEADERS ================= */
 // Service Worker caching
